45d57f17a6170f53de1b64b71c0c0c11c48dce95
Thiago <aquino.t@tutanota.com>
2023-08-21 10:16:01 -0300

    Calculator finalizado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,72 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let sum = 0;
+  args.forEach(element => {
+    if (typeof element == 'object') { 
+      for(let i = 0; i < args.length; i++) {
+          args[i].forEach(element => {
+            sum += element
+          });
+        }
+      }
+    else {
+      sum += element;
+    }})
+    
+    return parseInt(sum);
+    } 
+  
+      
+      
+        
+            
+          
+      
+
+const multiply = function(...args) {
+  let result = 1;
+  args.forEach(element => {
+    if (typeof element == 'object') { 
+      for(let i = 0; i < args.length; i++) {
+          args[i].forEach(element => {
+            result *= element
+          });
+        }
+      }
+    else {
+      result *= element;
+    }})
+    
+    return parseInt(result);
 };
 
-const multiply = function() {
-
+const power = function(base, pot) {
+  let result = base;
+  for (let i = 0; i < pot -1; i++) {
+    result *= base;
+  }
+  return result
 };
 
-const power = function() {
-	
+const factorial = function(fat) {
+  let n = fat - 1;
+  let result = 1;
+  
+	if (n > 0) {
+    result = fat * factorial(n)
+  }
+  return result;
 };
+    
 
-const factorial = function() {
-	
-};
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
