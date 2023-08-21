aa7c55dc0c4af49f53a5e91644b0ad2a39643467
Thiago <aquino.t@tutanota.com>
2023-08-21 11:24:07 -0300

    Exercicio de Palindromos feito

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[" ".,!?]/g, "");
+    
+    let upperString = newString.toUpperCase();
+    
+    let reverseString = upperString.split("").reverse().join("");
+    
+    if (upperString == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+palindromes("A car a man a maraca");
 
 // Do not edit below this line
 module.exports = palindromes;
