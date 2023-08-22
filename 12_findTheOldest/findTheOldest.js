8a352851c53110706b75576956c0d7f57a77e75a
Thiago <aquino.t@tutanota.com>
2023-08-22 11:11:19 -0300

    findTheOldest COncluido

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b) => {
+        const dataAtual = new Date();
+        const anoAtual = dataAtual.getFullYear();
+        let lastGuy = a.yearOfDeath - a.yearOfBirth;
+        let nextGuy = b.yearOfDeath - b.yearOfBirth;
+        if (!a.yearOfDeath) {
+            lastGuy = anoAtual - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            nextGuy = anoAtual - b.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
 };
+            
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
